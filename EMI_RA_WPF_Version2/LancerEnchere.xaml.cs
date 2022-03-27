2b973a6a35accb0163d7574d4018fe34f373d7c4
Cahuete0512 <74617561+Cahuete0512@users.noreply.github.com>
2022-03-27 17:37:18 +0200

    gestion de la sélection de la periode d'enchere limitée à 7 jours

--- EMI_RA_WPF_Version2/LancerEnchere.xaml.cs
+++ EMI_RA_WPF_Version2/LancerEnchere.xaml.cs
@@ -19,20 +19,18 @@ namespace EMI_RA_WPF
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            //HttpClient httpClient = new HttpClient();
-            //httpClient.BaseAddress = new Uri("https://localhost:5001/");
-
-
-            //HttpContent content = null;
-            //HttpResponseMessage httpResponse = httpClient.PostAsync("lancerEnchere", content).Result;
-
             var periodeDebut = OuvertureEnchere.SelectedDates[0];
             var periodeFin = OuvertureEnchere.SelectedDates[OuvertureEnchere.SelectedDates.Count-1];
 
+            if(OuvertureEnchere.SelectedDates.Count == 7) { 
             var clientapi = new Client("https://localhost:5001/", new HttpClient());
             await clientapi.LancerEnchereAsync(periodeDebut, periodeFin);
             MessageBox.Show($"la période d'enchère est validée, le mail est envoyé");
-
+            }
+            else
+            {
+                MessageBox.Show($"la période d'enchère est non-valide, vous devez sélectionner sept jours");
+            }
         }
         private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
